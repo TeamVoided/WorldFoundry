783543ef5a6a12b74696133222ea0eb26cb6c99c
TheEnderCore <theendercore@gmail.com>
2026-04-07 16:06:35 +0300

    fixed dark oak for good

--- src/main/java/org/teamvoided/world_foundry/mixin/DarkOakTrunkPlacerMixin.java
+++ src/main/java/org/teamvoided/world_foundry/mixin/DarkOakTrunkPlacerMixin.java
@@ -1,10 +1,6 @@
 package org.teamvoided.world_foundry.mixin;
 
-import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
-import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
-import net.minecraft.core.BlockPos;
-import net.minecraft.world.level.LevelSimulatedReader;
-import net.minecraft.world.level.levelgen.feature.TreeFeature;
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import net.minecraft.world.level.levelgen.feature.trunkplacers.DarkOakTrunkPlacer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -12,8 +8,8 @@ import org.spongepowered.asm.mixin.injection.At;
 @Mixin(DarkOakTrunkPlacer.class)
 public class DarkOakTrunkPlacerMixin {
 
-    @WrapOperation(method = "placeTrunk", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/levelgen/feature/TreeFeature;isAirOrLeaves(Lnet/minecraft/world/level/LevelSimulatedReader;Lnet/minecraft/core/BlockPos;)Z"))
-    boolean useTheFrickenCorrectCall(LevelSimulatedReader levelSimulatedReader, BlockPos blockPos, Operation<Boolean> original) {
-        return TreeFeature.isAirOrLeaves(levelSimulatedReader, blockPos);
+    @ModifyExpressionValue(method = "placeTrunk", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/levelgen/feature/TreeFeature;isAirOrLeaves(Lnet/minecraft/world/level/LevelSimulatedReader;Lnet/minecraft/core/BlockPos;)Z"))
+    boolean useTheFrickenCorrectCall(boolean original) {
+        return true;
     }
 }
